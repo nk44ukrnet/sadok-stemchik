c55de0010fdff04c36a5334ead0affe1fee94e39
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2022-06-22 17:36:17 +0300

    development phase, added sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-window.addEventListener('DOContentLoaded', function () {
-
+window.addEventListener('DOMContentLoaded', function () {
+    //hamburger
+    const hamburgerBtn = document.querySelector('.header__hamburger');
+    if (hamburgerBtn) {
+        hamburgerBtn.addEventListener('click', e => {
+            let headerEl = e.target.closest('.header');
+            if (headerEl) {
+                headerEl.classList.toggle('menu_active');
+            }
+        })
+    }
 });
